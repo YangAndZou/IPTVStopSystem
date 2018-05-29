4f4cab47a9d437b07095b97c152a6e38d6ea9dee
zoulf <zoulf666@163.com>
2018-05-29 09:27:29 +0800

    index模板语言block

--- IPTVStopSystem/urls.py
+++ IPTVStopSystem/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^login/$', views.login),
+    url(r'^login$', views.login),
     url(r'^$', views.index),
     url(r'^(?P<program_name>[\s\S]*)/(?P<program_ip>[\s\S]*)/(?P<status>[\s\S]*)$',
         views.index),

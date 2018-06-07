faf90ddbba82069f929497b40a9f8cf4658668a8
zoulf <zoulf666@163.com>
2018-06-07 09:08:15 +0800

    关停功能完善

--- IPTVStopSystem/utils.py
+++ IPTVStopSystem/utils.py
@@ -6,17 +6,17 @@ import paramiko
 
 
 # paramiko ssh 连接封装
-def ssh_paramiko(ip, username, passwd, cmd):
+def ssh_paramiko(ip, port, username, passwd, cmd, sudo):
     result = {}
     try:
         ssh = paramiko.SSHClient()
         # 用于允许连接不在known_hosts名单中的主机
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=ip, port=22, username=username, password=passwd, timeout=5)
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        # 与服务器交互，输出Y，这里的交互是指后面的cmd需要的执行的程序可能
-        # 出现交互的情况下，可以通过该参数进行交互。
-        # stdin.write("Y")
+        ssh.connect(hostname=ip, port=port, username=username, password=passwd, timeout=5)
+        # 当执行多条命令时，需要让get_pty=True，执行多条命令的格式为"cd /home;ls;cat z"
+        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True)
+        if sudo:
+            stdin.write(passwd + '\n')
         print(stdout.read())
         print('%s OK\n' % ip)
         ssh.close()
